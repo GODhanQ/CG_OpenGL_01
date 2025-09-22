7208eb0218d3e983bd21ee35dab8f1601986a45c
GODhanQ <hg.min2018@gmail.com>
2025-09-22 17:15:22 +0900

    Project 3 요구사항 재 확인 및 수정 완료

--- Project3/CG_OpenGL_01_test03.cpp
+++ Project3/CG_OpenGL_01_test03.cpp
@@ -1,12 +1,11 @@
 #include "VarANDFunc_test03.h"
 
-static std::vector<std::tuple<std::pair<float, float>, std::pair<float, float>, std::tuple<float, float, float>>> Rect_dir;
-static std::vector<std::tuple<std::pair<float, float>, std::pair<float, float>, std::tuple<float, float, float>>> Hide_Rect_dir;
-static std::vector<std::tuple<std::pair<float, float>, std::pair<float, float>, std::tuple<float, float, float>>> Big_Rect_dir;
+// Rect_dir : Bounds1, Bounds2, Color, origin
+static std::vector<std::tuple<std::pair<float, float>, std::pair<float, float>, std::tuple<float, float, float>, std::pair<float, float>>> Rect_dir;
 static constexpr int Window_width{ 700 }, Window_height{ 700 };
 static auto seed = std::chrono::system_clock::now().time_since_epoch().count();
 std::default_random_engine dre(static_cast<unsigned int>(seed));
-std::uniform_int_distribution<int> uid_color(0, 255);
+std::uniform_real_distribution<float> uid_color(0.0f, 1.0f);
 std::uniform_int_distribution<int> uid_window(-Window_width, Window_width);
 
 void main(int argc, char** argv)
@@ -45,13 +44,6 @@ GLvoid drawScene() {
 		glColor3f(std::get<0>(Rect_color), std::get<1>(Rect_color), std::get<2>(Rect_color));
 		glRectf(Rect_Bounds1.first, Rect_Bounds1.second, Rect_Bounds2.first, Rect_Bounds2.second);
 	}
-	for (int i = 0; i < Big_Rect_dir.size(); ++i) {
-		auto Rect_color{ std::get<2>(Big_Rect_dir[i]) };
-		auto &Rect_Bounds1{ std::get<0>(Big_Rect_dir[i]) };
-		auto &Rect_Bounds2{ std::get<1>(Big_Rect_dir[i]) };
-		glColor3f(std::get<0>(Rect_color), std::get<1>(Rect_color), std::get<2>(Rect_color));
-		glRectf(Rect_Bounds1.first, Rect_Bounds1.second, Rect_Bounds2.first, Rect_Bounds2.second);
-	}
 
 	glutPostRedisplay();
 	glutSwapBuffers();
@@ -66,11 +58,17 @@ void MouseDrag(int x, int y) {
 	auto ogl_xy = ConvertMouseWxy2GLxy(x, y);
 	float mouse_x = ogl_xy.first;
 	float mouse_y = ogl_xy.second;
-	float half_size = 0.1f;
-	std::get<0>(Rect_dir[DraggingRectIndex]).first = mouse_x - half_size;
-	std::get<1>(Rect_dir[DraggingRectIndex]).first = mouse_x + half_size;
-	std::get<0>(Rect_dir[DraggingRectIndex]).second = mouse_y + half_size;
-	std::get<1>(Rect_dir[DraggingRectIndex]).second = mouse_y - half_size;
+
+	float half_size_X = (std::get<1>(Rect_dir[DraggingRectIndex]).first - std::get<0>(Rect_dir[DraggingRectIndex]).first) / 2.0f;
+	float half_size_Y = (std::get<0>(Rect_dir[DraggingRectIndex]).second - std::get<1>(Rect_dir[DraggingRectIndex]).second) / 2.0f;
+
+	std::get<0>(Rect_dir[DraggingRectIndex]).first = mouse_x - half_size_X;
+	std::get<1>(Rect_dir[DraggingRectIndex]).first = mouse_x + half_size_X;
+	std::get<0>(Rect_dir[DraggingRectIndex]).second = mouse_y + half_size_Y;
+	std::get<1>(Rect_dir[DraggingRectIndex]).second = mouse_y - half_size_Y;
+
+
+
 	glutPostRedisplay();
 }
 
@@ -113,14 +111,10 @@ void MouseClick(int button, int state, int x, int y) {
 						float new_y1 = std::max(DraggedRect_Bounds1.second, OtherRect_Bounds1.second);
 						float new_x2 = std::max(DraggedRect_Bounds2.first, OtherRect_Bounds2.first);
 						float new_y2 = std::min(DraggedRect_Bounds2.second, OtherRect_Bounds2.second);
+						float r = uid_color(dre), g = uid_color(dre), b = uid_color(dre);
+						float mid_x = (new_x1 + new_x2) / 2.0f, mid_y = (new_y1 + new_y2) / 2.0f;
+						Rect_dir.push_back({ {new_x1, new_y1}, {new_x2, new_y2}, {r, g, b}, {mid_x, mid_y} });
 
-						float r = static_cast<float>(uid_color(dre)) / 255.0f;
-						float g = static_cast<float>(uid_color(dre)) / 255.0f;
-						float b = static_cast<float>(uid_color(dre)) / 255.0f;
-
-						Big_Rect_dir.push_back({ {new_x1, new_y1}, {new_x2, new_y2}, {r, g, b} });
-						Hide_Rect_dir.push_back(Rect_dir[DraggingRectIndex]);
-						Hide_Rect_dir.push_back(Rect_dir[i]);
 						if (i < DraggingRectIndex) {
 							Rect_dir.erase(Rect_dir.begin() + DraggingRectIndex);
 							Rect_dir.erase(Rect_dir.begin() + i);
@@ -138,38 +132,48 @@ void MouseClick(int button, int state, int x, int y) {
 		}
 	}
 	if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
-		// if right click and there is hidden rect, It is divided into two rectangles that were originally within that range.
-		int DraggingBigRectIndex = -1;
-		for (int i = 0; i < Big_Rect_dir.size(); ++i) {
+		for (int i = 0; i < Rect_dir.size(); ++i) {
+			auto& Rect = Rect_dir[i];
+			auto& Rect_Bounds1 = std::get<0>(Rect);
+			auto& Rect_Bounds2 = std::get<1>(Rect);
+			auto& Rect_color = std::get<2>(Rect);
+
 			auto ogl_xy = ConvertMouseWxy2GLxy(x, y);
 			float mouse_x = ogl_xy.first;
 			float mouse_y = ogl_xy.second;
-			auto& BigRect_Bounds1 = std::get<0>(Big_Rect_dir[i]);
-			auto& BigRect_Bounds2 = std::get<1>(Big_Rect_dir[i]);
-			if (mouse_x >= BigRect_Bounds1.first && mouse_x <= BigRect_Bounds2.first &&
-				mouse_y >= BigRect_Bounds2.second && mouse_y <= BigRect_Bounds1.second) {
-				DraggingBigRectIndex = i;
-				std::cout << "Selected Big Rect " << i << " for dividing." << std::endl;
+
+			if (mouse_x >= Rect_Bounds1.first && mouse_x <= Rect_Bounds2.first &&
+				mouse_y >= Rect_Bounds2.second && mouse_y <= Rect_Bounds1.second) {
+				std::cout << "Erased Rect " << i << "." << std::endl;
+
+				std::uniform_real_distribution<float> urd_SmallRect_origin_X(Rect_Bounds1.first, Rect_Bounds2.first);
+				std::uniform_real_distribution<float> urd_SmallRect_origin_Y(Rect_Bounds2.second, Rect_Bounds1.second);
+				std::uniform_real_distribution<float> urd_Color(0.0f, 1.0f);
+
+				for (int i = 0; i < 2; ++i) {
+					float mid_x = urd_SmallRect_origin_X(dre);
+					float mid_y = urd_SmallRect_origin_Y(dre);
+
+					float max_half_X = std::min(mid_x - Rect_Bounds1.first, Rect_Bounds2.first - mid_x);
+					std::uniform_real_distribution<float> urd_SmallRect_half_X(0.05f, max_half_X);
+
+					float max_half_Y = std::min(mid_y - Rect_Bounds2.second, Rect_Bounds1.second - mid_y);
+					std::uniform_real_distribution<float> urd_SmallRect_half_Y(0.05f, max_half_Y);
+
+					float half_size_X = urd_SmallRect_half_X(dre);
+					float half_size_Y = urd_SmallRect_half_Y(dre);
+					float x1 = mid_x - half_size_X, y1 = mid_y + half_size_Y, x2 = mid_x + half_size_X, y2 = mid_y - half_size_Y;
+					float r = urd_Color(dre), g = urd_Color(dre), b = urd_Color(dre);
+
+					Rect_dir.push_back({ {x1, y1}, {x2, y2}, {r, g, b}, {mid_x, mid_y} });
+				}
+				
+				Rect_dir.erase(Rect_dir.begin() + i);
 				break;
 			}
 		}
 
-		if (DraggingBigRectIndex != -1) {
-			std::cout << "Dividing Big Rect " << DraggingBigRectIndex << " back into original rectangles." << std::endl;
-			auto& BigRect = Big_Rect_dir[DraggingBigRectIndex];
-			Big_Rect_dir.erase(Big_Rect_dir.begin() + DraggingBigRectIndex);
-			for (int i = 0; i < Hide_Rect_dir.size(); ++i) {
-				auto& HiddenRect = Hide_Rect_dir[i];
-				auto& HiddenRect_Bounds1 = std::get<0>(HiddenRect);
-				auto& HiddenRect_Bounds2 = std::get<1>(HiddenRect);
-				if (!(HiddenRect_Bounds2.first < std::get<0>(BigRect).first || HiddenRect_Bounds1.first > std::get<1>(BigRect).first ||
-					HiddenRect_Bounds2.second > std::get<0>(BigRect).second || HiddenRect_Bounds1.second < std::get<1>(BigRect).second)) {
-					Rect_dir.push_back(HiddenRect);
-					Hide_Rect_dir.erase(Hide_Rect_dir.begin() + i);
-					--i;
-				}
-			}
-		}
+		glutPostRedisplay();
 	}
 }
 
@@ -193,11 +197,14 @@ void Keyboard(unsigned char key, int x, int y) {
 			float mid_x = static_cast<float>(uid_window(dre)) / Window_width;
 			float mid_y = static_cast<float>(uid_window(dre)) / Window_height;
 
-			float half_size = 0.1f;
-			float x1 = mid_x - half_size, y1 = mid_y + half_size, x2 = mid_x + half_size, y2 = mid_y - half_size;
-			float r = static_cast<float>(uid_color(dre)) / 255.0f, g = static_cast<float>(uid_color(dre)) / 255.0f, b = static_cast<float>(uid_color(dre)) / 255.0f;
+			std::uniform_real_distribution<float> uid_half_size_X(0.05f, 0.2f);
+			std::uniform_real_distribution<float> uid_half_size_Y(0.05f, 0.2f);
+			float half_size_X = uid_half_size_X(dre);
+			float half_size_Y = uid_half_size_Y(dre);
+			float x1 = mid_x - half_size_X, y1 = mid_y + half_size_Y, x2 = mid_x + half_size_X, y2 = mid_y - half_size_Y;
+			float r = uid_color(dre), g = uid_color(dre), b = uid_color(dre);
 
-			Rect_dir.push_back({ {x1, y1}, {x2, y2}, {r, g, b} });
+			Rect_dir.push_back({ {x1, y1}, {x2, y2}, {r, g, b}, {mid_x, mid_y} });
 		}
 		else std::cout << "Maximum number of Rect reached." << std::endl;
 		break;
